374a1062a2fcd5673f6516e0696a9c2dc43b55b1
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-06-25 10:38:36 -0500

    Name Selector

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -1,14 +1,39 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
 using System.Threading;
 
 class EntryPoint
 {
     static void Main()
     {
-        IWebDriver driver = new ChromeDriver(); // Inicialización del driver para abrir en este caso Google Chrome
-        driver.Navigate().GoToUrl("http://testing.todorvachev.com/");//Abrir la pagina http://testing.todorvachev.com/
-        Thread.Sleep(3000); // Esperar 3 Segundos
-        driver.Quit(); // Cerrar el Navegador
+        IWebDriver browser = new ChromeDriver(); // Inicialización del driver para abrir en este caso Google Chrome
+        browser.Navigate().GoToUrl("http://testing.todvachev.com/selectors/name/"); // Abrir la página
+        IWebElement element = browser.FindElement(By.Name("myName")); //Buscar el elemento por su nombre
+        if (element.Displayed)//Asking if the element is visible
+        {
+            GreenMessage("Is Visible");
+        }
+        else
+        {
+            RedMessage("Is not Visible");
+        }
+        Thread.Sleep(2000);
+        browser.Quit();
+    }
+
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
     }
 }
