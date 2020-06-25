dc04979e238fffb3ef5e15d82fa67043c0313faa
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-06-25 12:43:36 -0500

    CheckBox Element

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -7,28 +7,38 @@ using System.Threading;
 class EntryPoint
 {
     static IWebDriver browser = new ChromeDriver(); // Inicialización del driver para abrir en este caso Google Chrome
-    static IWebElement textBox;
+    static IWebElement checkBox;
+    
     static void Main()
     {
-        string url = "http://testing.todvachev.com/special-elements/text-input-field/";
+        string url = "http://testing.todvachev.com/special-elements/check-button-test-3/";
+        string option = "3";
         
         browser.Navigate().GoToUrl(url); // Abrir la página
-        string en1 = "username";
+        string en1 = "#post-33 > div > p:nth-child(8) > input[type=\"checkbox\"]:nth-child("+option+")";
+
         try
         {
-            textBox = browser.FindElement(By.Name(en1));
-            textBox.SendKeys("Test text");
-
+            checkBox = browser.FindElement(By.CssSelector(en1));
+            if (checkBox.GetAttribute("checked") == "true")
+            {
+                GreenMessage("The box "+option+" is checked");
+            }
+            else
+            {
+                RedMessage("The box is not checked");
+            }
+            Console.WriteLine(checkBox.GetAttribute("value"));
+            option = "1";
+            checkBox = browser.FindElement(By.CssSelector("#post-33 > div > p:nth-child(8) > input[type=\"checkbox\"]:nth-child(" + option + ")"));
+            Console.WriteLine(checkBox.GetAttribute("value"));
             Thread.Sleep(3000);
-
-            Console.WriteLine(textBox.GetAttribute("maxlength"));            
-
-            Thread.Sleep(3000);
-
+            checkBox.Click();
+            Thread.Sleep(2000);
         }
         catch (NoSuchElementException)
         {
-            RedMessage("No se encontró el elemento"+en1);
+            RedMessage("Can't Find the element");
         }     
                 
         Console.WriteLine("Press any Key to continue...");
