8bc604e4e08d919315e7e18bf116854a306831c7
LESTEBANRR <l_esteban_1998@hotmail.com>
2020-06-25 10:24:51 -0500

    Testing Selenium

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -1,7 +1,14 @@
-﻿class EntryPoint
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+class EntryPoint
 {
     static void Main()
     {
-
+        IWebDriver driver = new ChromeDriver(); // Inicialización del driver para abrir en este caso Google Chrome
+        driver.Navigate().GoToUrl("http://testing.todorvachev.com/");//Abrir la pagina http://testing.todorvachev.com/
+        Thread.Sleep(3000); // Esperar 3 Segundos
+        driver.Quit(); // Cerrar el Navegador
     }
 }
